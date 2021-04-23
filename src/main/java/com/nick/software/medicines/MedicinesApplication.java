b0f8aa1d5eb20dd286c7bf429658d0d4efbaa279
Nikolay Kotlyarov <nikolay.kotlyarov1@gmail.com>
2021-04-23 09:49:37 +0600

    init

---
+++ src/main/java/com/nick/software/medicines/MedicinesApplication.java
@@ -0,0 +1,13 @@
+package com.nick.software.medicines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicinesApplication.class, args);
+    }
+
+}
