31f70665d4de68b7bc05a8846cf347e7ad7fd29b
Nikolay Kotlyarov <nikolay.kotlyarov1@gmail.com>
2021-04-25 13:17:23 +0600

    login added

--- src/main/java/com/nick/software/medicines/account/controller/AccountController.java
+++ src/main/java/com/nick/software/medicines/account/controller/AccountController.java
@@ -1,4 +1,23 @@
 package com.nick.software.medicines.account.controller;
 
+import com.nick.software.medicines.account.service.AccountService;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Data
+@Controller
+@RequestMapping("/")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("register")
+    public String register(){
+
+        return "index";
+    }
 }
